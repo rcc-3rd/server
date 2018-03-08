b9f8a58542c08c20089c1964b60c1f2ee6e80bb7
tatsuaki85 <tatsuaki85@ritscc-macbook.local>
2018-03-08 23:12:24 +0900

    bugbug

--- src/beacon_allocator.rb
+++ src/beacon_allocator.rb
@@ -1,12 +1,35 @@
 require 'singleton'
 
+room_list = ["1","2","3","4","5"]
 
 class BeaconAcceptor
   include Singleton
 
-  def allocate_event (json) 
-    
+  def allocate_event (event) 
+    if room_list.any? {|w| w == event[:hwid]} then
+      #Roomが存在した場合
+      join_room(event[:hwid])
+    else
+      #Roomが存在しない場合
+      create_room(event[:hwid])
+    end
+
   end
 
 
+
+  def join_room(room_id)
+
+  end
+
+
+
+
+  def create_room(room_id)
+    room_list.push("#{room_id}")
+
+
+  end
+
 end
+
