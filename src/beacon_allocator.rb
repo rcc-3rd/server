97633566f92fc352dc4770fde5fe33788296b9bb
tatsuaki85 <tatsuaki85@ritscc-macbook.local>
2018-03-09 01:04:08 +0900

    ルーム作成・参加機構

--- src/beacon_allocator.rb
+++ src/beacon_allocator.rb
@@ -1,34 +1,37 @@
 require 'singleton'
 
-room_list = ["1","2","3","4","5"]
-
-class BeaconAllocator
+class BeaconAcceptor
   include Singleton
 
   def allocate_event (event) 
-    if room_list.any? {|w| w == event[:hwid]} then
-      #Roomが存在した場合
-      join_room(event[:hwid])
+
+    #user_idにuserをセット
+    user_id = ecent[:user]
+
+    #beabom_idをroom_idとする
+    room_id = event[:hwid]
+    if line_allocator.rooms.any? {|w| w == room_id} then
+      #roomが存在した場合
+      join_room(room_id)
+      participants_list = get_users(room_id)
     else
-      #Roomが存在しない場合
-      create_room(event[:hwid])
+      #roomが存在しない場合
+      create_room(room_id)
+      join_room(room_id)
+      participant_list = [user_id]
     end
-
   end
 
-
-
-  def join_room(room_id)
-
+  def create_room(room_id,user_id)
+    rooms << Room.new(room_id,user_id)
   end
 
+  def join_room(room_id,user_id)
+    rooms[room_id].users << user_id
+  end
 
-
-
-  def create_room(room_id)
-    room_list.push("#{room_id}")
-
-
+  def get_users(room_id)
+    return rooms[room_id].users
   end
 
 end
