221a3951262e714b2b9cb948df63793ba5996709
tatsuaki85 <tatsuaki85@ritscc-macbook.local>
2018-03-09 02:20:28 +0900

    参加者のリストを送信機能

--- src/beacon_allocator.rb
+++ src/beacon_allocator.rb
@@ -1,12 +1,13 @@
 require 'singleton'
 
+
 class BeaconAcceptor
   include Singleton
 
   def allocate_event (event) 
 
     #user_idにuserをセット
-    user_id = ecent[:user]
+    user_id = event[:userId]
 
     #beabom_idをroom_idとする
     room_id = event[:hwid]
@@ -14,11 +15,13 @@ class BeaconAcceptor
       #roomが存在した場合
       join_room(room_id)
       participants_list = get_users(room_id)
+      send_participants_list(participants_list)
     else
       #roomが存在しない場合
       create_room(room_id)
       join_room(room_id)
       participant_list = [user_id]
+      send_participants_list(user_id,participants_list)
     end
   end
 
@@ -27,12 +30,16 @@ class BeaconAcceptor
   end
 
   def join_room(room_id,user_id)
-    rooms[room_id].users << user_id
+    rooms.find{|tmp| tmp.beacon == room_id }.users << user_id
   end
 
   def get_users(room_id)
-    return rooms[room_id].users
+    participant_list = rooms.find{|tmp| tmp.beacon == room_id }.users
+    push_message(user_id, message_hash)
   end
 
+  def send_participants_list(user_id,participants_list)
+    message_hash = {"type":"text","text": participants_list}
+    $message_helper.push_message(user_id, message_hash)
+  end
 end
-
